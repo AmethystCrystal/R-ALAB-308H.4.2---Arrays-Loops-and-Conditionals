275d954056be2c65e63cb66ed82da44a8ac4e779
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2023-10-20 21:34:07 -0400

    Completed 3 problem statements.

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-// Easy Going
+// Write a for loop that will log the numbers 1 through 20.
 
-for (let i = 0; i <= 20; i++ ){
+// for (let i = 1; i <= 20; i++ ){
+//     console.log(i);
+// }
 
-}
+// Write a for loop that will log only the even numbers in 0 through 200.
+
+// for (let i = 0; i <= 200; i++){
+//     if (i % 2 === 0) console.log(i)
+    
+// }
+
+// Fizz Buzz
+
+// for (let i = 1; i <= 100; i++){
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//     }
+//     else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     }
+//     else if (i % 3 === 0) {
+//        console.log("Fizz"); 
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+
+ 
+ 
